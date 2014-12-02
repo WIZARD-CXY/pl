2e7879011ffc35f86617e7b63e6b86b312fa7510
wizard <wizardcxy@126.com>
2014-12-02 23:33:43 +0800

    add easy

---
+++ validPalindrome.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isAlphaOrNum(char c){
+        if( (c >= 'a' && c <= 'z')  || (c >= '0' && c <= '9') || ( c >= 'A' && c <='Z' ) ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    bool isPalindrome(string s) {
+        if(s == ""){
+            return true;
+        }
+        
+        int q=s.size()-1;
+        int p=0;
+        bool flag=true;
+        
+        while(p<q){
+            
+            if(!isAlphaOrNum(s[p])){
+                p++;
+                continue;
+            }
+            if(!isAlphaOrNum(s[q])){
+                q--;
+                continue;
+            }
+            
+            if(s[q]>='A' && s[q] <='Z'){
+                s[q]=s[q]-'A'+'a';
+            }
+            
+            if(s[p]>='A' && s[p] <='Z'){
+                s[p]=s[p]-'A'+'a';
+            }
+            
+            if(s[p]!=s[q]){
+                flag=false;
+                break;
+            }
+            p++;
+            q--;
+        }
+        return flag;
+    }
+};
