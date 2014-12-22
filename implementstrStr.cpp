ca3c0aa7ed7bb1b608079aed48a41b5e015b0162
wizard <wizardcxy@126.com>
2014-12-22 23:52:43 +0800

    add easy

---
+++ implementstrStr.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(char *haystack, char *needle) {
+        int hsize = strlen(haystack);
+        int nsize = strlen(needle);
+        
+        for(int  i=0; i<=(hsize-nsize); i++){
+            int j;
+            for(j=0; j<nsize; j++){
+                if(haystack[j+i] != needle[j]){
+                    break;
+                }
+            }
+            if(j==nsize){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
