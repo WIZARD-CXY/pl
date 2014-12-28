1595711dff1cf46d3eb176857bbc84ba361e72e0
wizard <wizardcxy@126.com>
2014-12-28 13:20:19 +0800

    add roman2int

---
+++ longestcommonprefix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        if(strs.empty()){
+            return "";
+        }
+        
+        string prefix = strs[0];
+        
+        for(int i=1; i<strs.size(); i++){
+            if(strs[i].size() ==0 || prefix.size()==0){
+                return "";
+            }
+            
+            int minLen= min(prefix.size(),strs[i].size());
+            int j;
+            for(j=0; j<minLen; j++){
+                if(prefix[j]!=strs[i][j]){
+                    break;
+                }
+            }
+            prefix=prefix.substr(0,j);
+            
+        }
+        
+        return prefix;
+        
+    }
+};
