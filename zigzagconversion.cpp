eaf1676b7ad34450185882c5037ab41d47e80bb4
wizard <wizardcxy@126.com>
2014-12-30 12:59:01 +0800

    add easy

---
+++ zigzagconversion.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        vector<string> v(nRows);
+        
+        int i=0;
+        while(i<s.size()){
+            //first fill the first column in the vector;
+            
+            for(int j=0; i<s.size() && j<nRows; j++,i++){
+                v[j]+=s[i];
+            }
+            //then fill the slide in the vector
+            for(int j=nRows-2; j>0 && i<s.size();j--,i++){
+                v[j]+=s[i];
+            }
+        }
+        
+        string res;
+        for(int i=0; i<nRows; i++){
+            for(int j=0; j<v[i].size(); j++){
+                res+=v[i][j];
+            }
+        }
+        
+        return res;
+        
+    }
+};
