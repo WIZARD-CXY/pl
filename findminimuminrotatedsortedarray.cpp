99c9f34894d68e26f487ed9b2c5934d508b913df
wizard <wizardcxy@126.com>
2015-01-11 13:23:23 +0800

    add binarysearch theory prob

---
+++ findminimuminrotatedsortedarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        
+        int l=0;
+        int r=num.size()-1;
+        
+        while(l<r){
+            int mid = (l+r)/2;
+            
+            if(num[mid]>num[r]){
+                l=mid+1;
+            }else {
+                r=mid;
+            }
+        }
+        return num[l];
+        
+    }
+};
