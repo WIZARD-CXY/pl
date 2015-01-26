2c75949eb1b02eb67e3b9aa7cde71ca3c1c8ec70
wizard <wizardcxy@126.com>
2015-01-26 12:29:00 +0800

    add largest number

---
+++ largestnumber.cpp
@@ -0,0 +1,31 @@
+bool cmp(const string &s1, const string &s2) {
+        return (s1 + s2) > (s2 + s1);
+}
+class Solution {
+public:
+
+    string largestNumber(vector<int> &num) {
+        vector<string> temp;
+    
+        for (int i = 0; i < num.size(); ++i) {
+            temp.push_back(to_string(num[i]));
+        }
+        sort(temp.begin(), temp.end(), cmp);
+        string res="";
+        
+        //when the first element is "0".
+        //the input num must be [0,0,0...]
+        //so just return "0"
+        if(temp[0]=="0"){
+            return "0";
+        }
+
+        int i=0; 
+        
+        while(i<temp.size()){
+            res+=temp[i];
+            i++;
+        }
+        return res;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,75 +1,43 @@
 #include<iostream>
 #include<string>
-
 #include<vector>
-
 #include<sstream>
 #include<algorithm>
 #include<limits.h>
 #include<stack>
 using namespace std;
 
-int stoi(string s){
-    int res=0;
-    int sign=1;
-
-    for(int i=0; i<s.size(); i++){
-        if(i==0 && s[i] == '-'){
-            sign=-1;
-        }else{
-            res=res*10+s[i]-'0';
-        }
-
-    }
-    return sign*res;
+bool cmp1(const string &a, const string &b){
+    return (a+b) > (b+a);
 }
 
-int evalRPN(vector<string> &tokens) {
-        stack<int> ss;
-        int temp1,temp2;
-        
-        for(int i=0; i<tokens.size(); i++){
-            
-            if(ss.empty() || (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")){
-                ss.push(stoi(tokens[i]));
-            }else if(tokens[i] == "+"){
-                temp1 = ss.top();
-                ss.pop();
-                temp2 = ss.top();
-                ss.pop();
-                ss.push(temp1+temp2);
-            }else if(tokens[i] == "-"){
-                temp1 = ss.top();
-                ss.pop();
-                temp2 = ss.top();
-                ss.pop();
-                ss.push(temp2-temp1);
-            }else if(tokens[i] == "*"){
-                temp1 = ss.top();
-                ss.pop();
-                temp2 = ss.top();
-                ss.pop();
-                ss.push(temp1*temp2);
-            }else if(tokens[i] == "/"){
-                temp1 = ss.top();
-                ss.pop();
-                temp2 = ss.top();
-                ss.pop();
-                ss.push(temp2/temp1);
-            }
-            
-        }
-        return ss.top();
-        
+string largestNumber(vector<int> &num) {
+    vector<string> temp;
+
+    for(int i=0; i<num.size(); i++){
+        temp.push_back(to_string(num[i]));
     }
+    
+    sort(temp.begin(),temp.end(),cmp1);
+    
+    string res="";
+    
+    for(int i=0; i<temp.size(); i++){
+        res+=temp[i];
+    }
+    
+    return res;
+        
+}
 
 
 int main(){
-    std::vector<string> v;
-    v.push_back("6");
-    v.push_back("3");
-    v.push_back("/");
-    cout<<evalRPN(v)<<endl;
-    cout<<stoi("012345")<<endl;
+    std::vector<int> v;
+    v.push_back(3);
+    v.push_back(30);
+    v.push_back(34);
+    v.push_back(5);
+    v.push_back(9);
+    cout<<largestNumber(v)<<endl;
 
 }
