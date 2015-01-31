20e61ee9c67029c04a77e5201d6b40b17add741f
wizard <wizardcxy@126.com>
2015-02-01 00:00:24 +0800

    add insertionSortlist

---
+++ insertionSortList.cpp
@@ -0,0 +1,12 @@
+class Solution{
+public:
+	ListNode *insertionSortedList(ListNode *head){
+		ListNode *sortedHead = new ListNode(-1);
+
+		while(head){
+			//keep the head position
+			ListNode *temp =head->next;
+			ListNode *cur = sortedHead;
+		}
+
+}
