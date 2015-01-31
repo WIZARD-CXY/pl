a9397dc6c15309eda7f506744e6b526219cb2122
wizard <wizardcxy@126.com>
2015-02-01 00:31:49 +0800

    a better version of insertionSortlist

--- insertionSortList.cpp
+++ insertionSortList.cpp
@@ -1,24 +1,24 @@
-class Solution{
+class Solution {
 public:
-	ListNode *insertionSortedList(ListNode *head){
-		ListNode *sortedHead = new ListNode(-1);
-
-		while(head){
-			//keep the head->next position
-			ListNode *temp =head->next;
-			ListNode *cur = sortedHead;
-
-			while(cur->next != NULL && cur->next->val < head->val){
-				cur=cur->next;
-			}
-			//insert head node into the list following cur node
-			head->next=cur->next;
-			cur->next=head;
-
-			//restore head
-			head=temp;
-		}
-		return sortedHead->next;
-	}
-
-}
+    ListNode *insertionSortList(ListNode *head) {
+        ListNode *dummy = new ListNode(-1);
+        
+        while(head){
+            
+            ListNode *cur = dummy;
+            
+            while(cur->next && cur->next->val < head->val){
+                cur=cur->next;
+            }
+            //save the head->next first
+            ListNode *temp = head->next;
+            //insert head into the list following cur
+            head->next=cur->next;
+            cur->next=head;
+            
+            // move forward
+            head=temp;
+        }
+        return dummy->next;
+    }
+};
