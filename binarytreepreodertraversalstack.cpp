719bd998ffbab13ddd5bf7d59b9f92e093e07627
wizard <wizardcxy@126.com>
2015-02-02 23:59:53 +0800

    iterative version of tree preOrder traversal

---
+++ binarytreepreodertraversalstack.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> res;
+        stack<TreeNode*> ss;
+        
+        if(root){
+            ss.push(root);
+            
+            while(!ss.empty()){
+                TreeNode *temp=ss.top();
+                res.push_back(temp->val);
+                ss.pop();
+                if(temp->right){
+                    ss.push(temp->right);
+                }
+                if(temp->left){
+                    ss.push(temp->left);
+                }
+            }
+        }
+        return res;
+    }
+    
+};
