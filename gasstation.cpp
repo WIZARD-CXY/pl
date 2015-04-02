11e0d2f071f11c2beca53659eaa65d3412e4c1a5
wizard <wizard_cxy@hotmail.com>
2015-04-02 09:19:40 +0800

    refactor the gasstation

--- gasstation.cpp
+++ gasstation.cpp
@@ -4,19 +4,16 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
         
-        int total=0,sum=0,start=0;
+        int total=0,sum=0,j=-1;
         
         for(int i=0; i<gas.size(); i++){
             total+=(gas[i]-cost[i]);
-            
+            sum+=(gas[i]-cost[i]);            
             if(sum<0){
-                sum=gas[i]-cost[i];
-                start=i;
-            }else{
-                sum+=(gas[i]-cost[i]);
-            }
-            
+                sum=0;
+                j=i;
+            } 
         }
-        return total<0?-1:start;     
+        return total<0?-1:j+1;     
     }
 };
