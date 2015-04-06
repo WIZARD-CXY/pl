564a03ae8c093c4180489e83d78753201f8cda77
Xingyu Chen <wizard_cxy@hotmail.com>
2015-04-07 00:26:07 +0800

    add clonegraph

---
+++ clonegraph.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+#define UNode UndirectedGraphNode
+class Solution {
+public:
+    unordered_map<UNode*, UNode* >::iterator iter;
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(node == NULL){
+            return NULL;
+        }
+        
+        UNode* nnode = new UNode(node->label);
+        
+        unordered_map<UNode*, UNode* > mm;
+        mm[node]=nnode;
+        
+        dfs(mm,node,nnode);
+        
+        return nnode;
+        
+    }
+    
+    void dfs(unordered_map<UNode*, UNode*> &mm, UNode* node, UNode* nnode){
+        for(int i=0; i != node->neighbors.size(); i++){
+            UNode * now = node->neighbors[i];
+            if((iter=mm.find(now))!=mm.end()){
+                //neibour pre exist just update the neighbors
+                nnode->neighbors.push_back(iter->second);
+            }else{
+                //cloned neighbor not existed before just create a new node and dfs on it
+                UNode *nnow = new UNode(now->label);
+                mm[now]=nnow;
+                nnode->neighbors.push_back(nnow);
+                dfs(mm,now,nnow);
+                
+            }
+        }
+    }
+};
