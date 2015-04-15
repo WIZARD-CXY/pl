5137c0e56db55c1d3fda729b10f5bcdf68b919f6
Xingyu Chen <wizard_cxy@hotmail.com>
2015-04-16 00:10:37 +0800

    add bfs for word ladder

---
+++ wordladder.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        if(start.empty() || end.empty()){
+            return 0;
+        }
+        if(start==end){
+            return 0;
+        }
+        
+        queue<string> path;
+        path.push(start);
+        int level=1;
+        
+        while(!path.empty() && dict.size()>0){
+            string temp = path.front();
+            path.pop();
+            for(int i=0; i<temp.size(); i++){
+                for(int j='a'; j<='z'; j++){
+                   if(temp[i]==j){
+                       continue;
+                   }
+                   
+                   temp[i]=j;
+                   if(temp==end){
+                       return level+1;
+                   }
+                   if(dict.find(temp)!=dict.end()){
+                       path.push(temp);
+                       dict.erase(temp);
+                   }
+                }
+                
+            }
+            
+        }
+        return 0;
+        
+    }
+};
