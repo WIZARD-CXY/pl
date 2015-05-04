ea6ac77202d399de8a5ddd5228a4750f67f8d250
wizard <wizard_cxy@hotmail.com>
2015-05-04 20:37:47 +0800

    add triangle

---
+++ triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+        if(triangle.size()==0){
+            return 0;
+        }
+        
+        int layer=triangle.size();
+        int largestSize=triangle[triangle.size()-1].size();
+        
+        int d[layer][largestSize];
+        for(int i=0; i<largestSize; i++){
+            d[layer-1][i]=triangle[layer-1][i];
+        }
+        
+        for(int i=layer-2; i>=0;i--){
+            for(int j=0; j<triangle[i].size(); j++){
+                d[i][j]=min(d[i+1][j],d[i+1][j+1])+triangle[i][j];
+            }
+        }
+        
+        return d[0][0];
+    }
+};
