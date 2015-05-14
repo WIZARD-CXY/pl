48c821fac0de71a0c4a01dd2049cc93265a774ee
wizard <wizard_cxy@hotmail.com>
2015-05-14 10:37:10 +0800

    add next permutation

---
+++ nextpermutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i,k;
+        for(i=nums.size()-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+        }
+        
+        if(i<0){
+            // already the last permutation  all non-increasing order,just reverse
+            return reverse(nums.begin(),nums.end());
+        }
+        for(k=nums.size()-1; k>i; k--){
+            if(nums[k]>nums[i]){
+                break;
+            }
+        }
+        
+        swap(nums[i],nums[k]);
+        reverse(nums.begin()+i+1,nums.end());
+    }
+};
