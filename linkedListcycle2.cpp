3a28a1f6e49af0031b4706bef436d1e6089be1ba
wizard <wizard_cxy@hotmail.com>
2015-05-20 20:59:59 +0800

    add linked list cycle

--- linkedListcycle2.cpp
+++ linkedListcycle2.cpp
@@ -9,32 +9,22 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(!head){
-            return NULL;
-        }
-        
-        ListNode *slow, *fast;
-        slow=fast=head;
+        ListNode *slow=head,*fast=head;
         
         while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast){
-                break;
+            
+            if(slow == fast){
+                ListNode *slow2 = head;
+                
+                while(slow!=slow2){
+                    slow=slow->next;
+                    slow2=slow2->next;
+                }
+                return slow2;
             }
         }
-        
-        if(!fast || !(fast->next)){
-            return NULL;
-            
-        }
-        
-        slow=head;
-        while(slow!=fast){
-            slow=slow->next;
-            fast=fast->next;
-        }
-        
-        return slow;
+        return NULL;
     }
 };
--- linkedlistcycle.cpp
+++ linkedlistcycle.cpp
@@ -9,19 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL){
-            return false;
-        }
+        ListNode *slow=head,*fast=head;
         
-        ListNode *slow=head;
-        ListNode *fast=head->next;
-        
-        while(fast!=NULL && fast->next!=NULL){
-            if(slow==fast){
-               return true;
-            }
+        while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
+            
+            if(slow==fast){
+                return true;
+            }
         }
         return false;
     }
