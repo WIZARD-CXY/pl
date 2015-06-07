c1ab9656fe239bebc1eca827888793026957ffb1
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-07 13:30:45 +0800

    fix code and look more generic

--- sqrt.cpp
+++ sqrt.cpp
@@ -5,23 +5,19 @@ public:
             return -1;
         }
         long long l = 0;
-        long long r = x/2 + 1;
+        long long r = x/2 + 2;
         
         while(l < r){
             long long mid = (l+r)/2;
             long long tmp = mid * mid;
             if(tmp == x) return mid;
-            else if(tmp > x) r = mid -1;
+            else if(tmp > x) r = mid;
             else l = mid + 1;
         }
         
-        // not found a little
-        long long temp=r*r;
+        // not found sqrt(x) directly. change a little bit
+        // l is the upper_bount of sqrt(n)
         
-        if(temp>x){
-            return r-1;
-        }else{
-            return r;
-        }
+        return l-1;
     }
 };
