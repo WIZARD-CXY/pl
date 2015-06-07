eac609cd1cd47f5beb5e59af1d6b147e6035632e
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-07 14:24:02 +0800

    add maximum path sum for bst

---
+++ binarytreemaximumpathsum.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxSum=INT_MIN;
+    int maxPathSum(TreeNode* root) {
+        pathSum(root);
+        
+        return maxSum;
+    }
+    
+    int pathSum(TreeNode *root){
+        if(root==NULL){
+            return 0;
+        }
+        
+        int lsum=pathSum(root->left);
+        int rsum=pathSum(root->right);
+        
+        int sum=root->val;
+        
+        if(lsum>0){
+            sum+=lsum;
+        }
+        
+        if(rsum>0){
+            sum+=rsum;
+        }
+         maxSum=max(maxSum,sum);
+         
+         return max(lsum,rsum)>0?max(lsum,rsum)+root->val:root->val;
+        
+    }
+};
