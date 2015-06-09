ba128f5dae4283552f15e5bea304fa4b09ce6da6
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-10 00:22:05 +0800

    add subset

--- subset.cpp
+++ subset.cpp
@@ -1,21 +1,14 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<sstream>
-#include<algorithm>
-#include<limits.h>
-#include<stack>
-#include<queue>
-using namespace std;
-
-vector<vector<int> > subsets(vector<int>& nums) {
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
         vector<vector<int> > res;
         int n=nums.size();
-        vector<int> temp;
+        
         
         for(int i=0;i<(1<<n); i++){
+            vector<int> temp;
             
             for(int j=0; j<n; j++){
                 if(i & (1<<j)){
@@ -24,14 +17,8 @@ vector<vector<int> > subsets(vector<int>& nums) {
             }
                 
             res.push_back(temp);
-            temp.clear();
-            
         }
+        
         return res;
     }
-int main(){
-   vector<int> a;
-   a.push_back(0);
-
-   subsets(a);
-}
+};
