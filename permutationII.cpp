11f3cb809fe536d85e52122910d7a5507b08154b
wizard <wizard_cxy@hotmail.com>
2015-06-10 13:25:51 +0800

    add permutation

---
+++ permutationII.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int> > res;
+    void solve(int n, vector<int> &temp, vector<int> &nums,vector<bool> &used){
+        if(temp.size()==n){
+            
+            res.push_back(temp);
+            return;
+        }
+        for(int i=0; i<nums.size(); i++){
+    
+            if(used[i] || (i!=0 && nums[i-1]==nums[i] && used[i-1]) ){
+                continue;
+            }
+    
+            temp.push_back(nums[i]);
+            used[i]=1;
+            solve(n,temp,nums,used);
+            used[i]=0;
+            temp.pop_back();  
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        vector<bool> used(nums.size(),0);
+        vector<int> temp;
+        
+        solve(nums.size(),temp,nums,used);
+        
+        return res; 
+        
+    }
+};
---
+++ permutation__.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int> > res;
+
+    void permutate(vector<int> &nums,vector<int> &temp){
+        if(temp.size()==nums.size()){
+           res.push_back(temp);
+           return;
+        }
+        for(int i=0; i<nums.size(); i++){
+            auto pos =find(temp.begin(),temp.end(),nums[i]);
+            
+            if(pos==temp.end()){
+                temp.push_back(nums[i]);
+                permutate(nums,temp);
+                temp.pop_back();
+            }
+            
+        }
+    }
+    vector<vector<int> > permute(vector<int>& nums) {
+        vector<int> temp;
+        permutate(nums,temp);
+        return res;
+    }
+};
