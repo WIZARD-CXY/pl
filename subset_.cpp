144c4c9eb95a41ab89302e0a134e09d6a5c57d87
wizard <wizard_cxy@hotmail.com>
2015-06-10 10:31:35 +0800

    add subset another sol

---
+++ subset_.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int> > res;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> path;
+        
+        helper(path,0,nums);
+        
+        return res;
+        
+    }
+    
+    void helper(vector<int> &path,int cur,vector<int> &nums){
+        if(cur==nums.size()){
+            res.push_back(path);
+            return;
+        }
+        
+        // not select the cur
+        helper(path,cur+1,nums);
+        
+        //select the cur
+        
+        path.push_back(nums[cur]);
+        helper(path,cur+1,nums);
+        path.pop_back();
+    }
+};
