741d34cd64596fa94d0f28fb6d5e51605a8a8fd3
wizard <wizard_cxy@hotmail.com>
2015-06-12 10:23:56 +0800

    fix wordladder2

--- wordladder2.cpp
+++ wordladder2.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
+    vector<vector<string> > res;
+    vector<string> temp_path;
+
     vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
-        vector<vector<string> > res;
+        
         if(start == end){
             return res;
         }
         
         unordered_set<string> current,next;
-        unordered_set<string> flag;
+        unordered_set<string> unvisited;
         unordered_map<string,vector<string> > father;
         
         current.insert(start);
-        
+
         bool found=false;
         
         while(!current.empty() && !found){
-            //expand
-            for(const auto &x: current){
-                flag.insert(x);
+
+            for(auto &x: current){
+                unvisited.insert(x);
             }
             
             for(auto x :current){
                 for(int i=0; i<x.size(); i++){
                     string temp=x;
+
                     for(char j='a'; j<='z'; j++){
                         if(temp[i]==j){
                             continue;
                         }
                         
                         temp[i]=j;
+                        
                         if(temp==end){
                             found=true;
                         }
-                        
-                        if(dict.find(temp)!=dict.end() && flag.find(temp)==flag.end()){
+                
+                        if(dict.find(temp)!=dict.end() && unvisited.find(temp)==unvisited.end()){
                             next.insert(temp);
                             father[temp].push_back(x);
                         }
@@ -42,32 +47,33 @@ public:
             }
             
             current=next;
+            next.clear();
         }
         
         if(found){
-            vector<string> c;
-            dfs(res,father,c,start,end);
+            dfs(father,start,end);
         }
         
         return res;
     }
     
-    void dfs(vector<vector<string> > &res, const unordered_map<string,vector<string> > &father, vector<string> &c,string &start,string &now){
-        c.push_back(now);
+    void dfs(const unordered_map<string,vector<string> > &father, string start, string now){
+        temp_path.push_back(now);
         
         if(now==start){
-            reverse(c.begin(),c.end());
-            res.push_back(c);
-            c.pop_back();
+            res.push_back(temp_path);
+            // do not get temp_path dirty, reverse it in the res.back
+            reverse(res.back().begin(),res.back().end());
+            temp_path.pop_back();
             return;
         }
         
         auto que = father.find(now)->second;
         
         for(auto x : que){
-            dfs(res,father,c,start,x);
+            dfs(father,start,x);
         }
         
-        c.pop_back();
+        temp_path.pop_back();
     }
 };
