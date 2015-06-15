353283e08b1d4ff0241fe1c597c737682cceff0d
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-16 00:11:01 +0800

    add 2queue to stack

---
+++ implementstackusing2queue.cpp
@@ -0,0 +1,38 @@
+class Stack {
+public:
+    queue<int> qq[2];
+    int cur=0;
+    // Push element x onto stack.
+    void push(int x) {
+        qq[cur].push(x);
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+        while(qq[cur].size()>1){
+            qq[1-cur].push(qq[cur].front());
+            qq[cur].pop();
+        }
+        qq[cur].pop();
+        cur=1-cur;
+    }
+
+    // Get the top element.
+    int top() {
+        while(qq[cur].size()>1){
+            qq[1-cur].push(qq[cur].front());
+            qq[cur].pop();
+        }
+        int x = qq[cur].front();
+        qq[1-cur].push(x);
+        qq[cur].pop();
+        cur=1-cur;
+        
+        return x;
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        return qq[cur].empty();
+    }
+};
