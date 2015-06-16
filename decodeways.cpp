14bb700ff147b04b80fff6de5178cf95c2718283
wizard <wizard_cxy@hotmail.com>
2015-06-16 23:04:54 +0800

    add decodeways

---
+++ decodeways.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n==0){
+            return 0;
+        }
+        
+        int f[n+1];
+        f[0]=1;
+        
+        if(s[0]=='0'){
+            f[1]=0;
+        }else{
+            f[1]=1;
+        }
+        
+        for(int i=2; i<=n; i++){
+            if(s[i-1]=='0'){
+                // if s[i-1] is '0' can't form with the previous answer
+                f[i]=0;
+            }else{
+                //else first join the previus answer
+                f[i]=f[i-1];
+            }
+            // if the current one and previous one can join to 10<=num<=26, then f[i] can be joined with f[i-2]
+            if(s[i-2]=='1' || (s[i-2]=='2' && s[i-1]<='6')){
+                f[i]+=f[i-2];
+            }
+        }
+        
+        return f[n];
+    }
+};
--- restoreip.cpp
+++ restoreip.cpp
@@ -1,30 +1,52 @@
-// search in step-th step with start index 'start', 
-    void dfs(string &s, string ip, int start, int step){
-        if(start==s.size() && step ==4){
-            ip.resize(ip.size()-1);
-            res.push_back(ip);
-        }
-        
-        if(s.size()-start > (4-step)*3){
-            return; //return early because there is too many extra bits
-        }
-        
-        if(s.size()-start <(4-step)){
-            return; //return early because there is not enough bits
-        }
-        
-        int num=0;
-        for(int i=start; i<start+3; i++){
-            num=num*10+s[i]-'0';
+class Solution {
+public:
+      bool isValid(string s){
+          if(s.size()==1 && s>="0" && s<="9") return true;
+          if(s.size()==2 && s>="10" && s<="99") return true;
+          if(s.size()==3 && s>="100" && s<="255") return true;
+          
+          return false;
+      }
+        vector<string> res;
+        vector<string> restoreIpAddresses(string s) {
+            vector<string> ipSegs;
             
-            if(num<=255){
-                // search forward
-                ip+=s[i];   
-                dfs(s, ip+'.', i+1, step+1);
+            dfs(s, ipSegs, 0);
+            
+            return res;
+        }
+        
+        // search in step-th step with start index 'start', 
+        void dfs(string &s, vector<string> &ipSegs, int start){
+            int step=ipSegs.size();
+            
+            if(start==s.size() && step ==4){
+                string temp=ipSegs[0];
+                
+                for(int i=1; i<4; i++){
+                    temp=temp+"."+ipSegs[i];
+                }
+                
+                res.push_back(temp);
+                return;
             }
             
-            if(num==0){
-                break;// do not like leading 0
+            if(s.size()-start > (4-step)*3){
+                return; //return early because there is too many extra bits
+            }
+            
+            if(s.size()-start <(4-step)){
+                return; //return early because there is not enough bits
+            }
+            
+            for(int i=1; i<=3 && start+i-1<s.size(); i++){
+                string temp=s.substr(start,i);
+                if(isValid(temp)){
+                    ipSegs.push_back(temp);
+                    //search forward
+                    dfs(s, ipSegs,start+i);
+                    ipSegs.pop_back();
+                }
             }
         }
-    }
+};
