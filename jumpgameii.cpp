a8d0e484e739d699d38d3f2e591230dd33b08cfe
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-18 23:26:19 +0800

    add jumpgame 2

---
+++ jumpgameii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        
+        int curReach=0;// the largest distance that has been reached
+        int curMax=0; // the largest distance that can reach by taking ret+1 step
+        int res=0; // the current jump number
+        
+        for(int i=0; i<n; i++){
+            if(curReach<i){
+                // if the current farthest reach can not get i, need  take another jump to get to curMax
+                res++;
+                curReach=curMax;
+            }
+            // every time try to update the largest jump distance
+            curMax=max(curMax,i+nums[i]);
+        }
+        
+        return res;
+        
+    }
+};
