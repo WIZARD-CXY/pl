3284cbe569bc0c71c678f3633fa09ff5463163d5
Xingyu Chen <wizard_cxy@hotmail.com>
2015-06-21 22:15:39 +0800

    add reverse Integer

--- test.cpp
+++ test.cpp
@@ -8,59 +8,27 @@
 #include<queue>
 using namespace std;
 
-vector<vector<int> > res;
-
-void dfs(vector<int> &candidates, vector<int> &path, int start, int target){
-        if(target==0){
-            res.push_back(path);
-            return;
-        }
+class Solution {
+public:
+    int reverse(int x) {
+        long long  res=0;
         
-        int previous=-1;
-        
-        for(int i=start ; i<candidates.size(); i++){
-            if(target < candidates[i]){
-                return; //prune
+        while(x){
+            res=res*10+x%10;
+            if(res>INT_MAX){
+                return 0;
+            }else if(res<INT_MIN){
+                return 0;
             }
-            
-            if(candidates[i]==previous){
-                continue;
-            }
-            path.push_back(candidates[i]);
-            previous=candidates[i];
-            dfs(candidates,path,i+1,target-candidates[i]);
-            path.pop_back();
+            x/=10;
         }
-    }
-    
-    vector<vector<int> > combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        
-        vector<int> path;
-        dfs(candidates,path,0,target);
         
         return res;
+        
     }
-    
-    
-    
+};
+
 int main(){
-
-    vector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(2);
-    vec.push_back(2);
-    vec.push_back(4);
-
-   combinationSum2(vec,7);
-
-   for(int i=0; i<res.size();i++){
-    for(int j=0; j<res[i].size(); j++){
-        cout<<res[i][j]<<" ";
-
-    
-   }
-   cout<<endl;
-}
+    Solution sln;
+    cout<<sln.reverse(-1234);
 }
