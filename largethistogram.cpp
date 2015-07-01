c36d0ce5de8384b59e2f142e0597a30c293b8244
Xingyu Chen <wizard_cxy@hotmail.com>
2015-07-01 10:53:11 +0800

    add largest histogram

---
+++ largethistogram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        int res=0;
+        stack<int> ss;
+        
+        int i=0;
+        height.push_back(0);// one dummy element
+        
+        while(i<height.size()){
+            if(ss.empty()||height[i]>=height[ss.top()]){
+                //push current idx into stack and move forward
+                ss.push(i++);
+            }else{
+                //pop the stack and compute all the candidates
+                int t=ss.top();
+                ss.pop();
+                res=max(res,height[t]*(ss.empty()?i:i-ss.top()-1));
+            }
+        }
+        return res;
+        
+    }
+};
