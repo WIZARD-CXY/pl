7b11ad5f84b9f42971082e4f6d6c0c2f88165a9d
wizard <wizard_cxy@hotmail.com>
2015-07-02 11:20:13 +0800

    add countone

---
+++ lintcode/countones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    /**
+     * @param num: an integer
+     * @return: an integer, the number of ones in num
+     */
+    int countOnes(int num) {
+        // write your code here
+        int res=0;
+        
+        while(num){
+            num=num&(num-1);
+            res++;
+        }
+        
+        return res;
+    }
+};
