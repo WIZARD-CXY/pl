206685ad6729a5825c5ced131b28411a85bc7175
Xingyu Chen <wizard_cxy@hotmail.com>
2015-07-08 23:01:27 +0800

    add stack2queue

---
+++ leetcode/implementqueueusestack.cpp
@@ -0,0 +1,49 @@
+class Queue {
+public:
+    // Push element x to the back of queue.
+    void push(int x) {
+        stack<int> temp;
+        while(!s.empty()){
+            //pop all the element to temp
+            temp.push(s.top());
+            s.pop();
+        }
+        s.push(x);
+        
+        //pop back
+        while(!temp.empty()){
+            s.push(temp.top());
+            temp.pop();
+        }
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+        s.pop();
+    }
+
+    // Get the front element.
+    int peek(void) {
+        return s.top();
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return s.empty();
+    }
+    
+    // return the last element
+    int back() {
+
+        stack<int> temp;
+        while(!s.empty()){
+            //pop all the element to temp
+            temp.push(s.top());
+            s.pop();
+        }
+        return temp.top();
+
+    }
+private:
+    stack<int> s;
+};
