b0568729ca3cca39b3b57b3f5dcb4f4b3bff077c
Xingyu Chen <wizard_cxy@hotmail.com>
2015-07-14 16:04:22 +0800

    add more

---
+++ lintcode/fastpower.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    /*
+     * @param a, b, n: 32bit integers
+     * @return: An integer
+     */
+    int fastPower(int a, int b, int n) {
+        // write your code here
+        if(n==0){
+            return 1%b;
+        }
+        if(n==1){
+            return a%b;
+        }
+        
+        //if b is
+        long long res=fastPower(a,b,n>>1)%b;
+        
+        res=(res*res)%b;
+        
+        if(n&1){
+            res=res*a%b;
+        }
+    
+        return res;
+    }
+};
