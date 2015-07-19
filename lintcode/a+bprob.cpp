8320601933360ffe0d71c562edae62ce10a00c20
Xingyu Chen <wizard_cxy@hotmail.com>
2015-07-20 00:29:50 +0800

    add more

---
+++ lintcode/a+bprob.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    /*
+     * @param a: The first integer
+     * @param b: The second integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        // write your code here, try to do it without arithmetic operators.
+        while(b!=0){
+            int carry=(a&b)<<1;
+            a=a^b;
+            b=carry;
+        }
+        return a;
+    }
+};
