5eadb4d71b60ff46fdbd180232becb7ec2754419
Xingyu Chen <wizard_cxy@hotmail.com>
2015-08-07 13:50:29 +0800

    add more

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -13,55 +13,101 @@ using namespace std;
 class Solution {
 public:
     /**
-     * @param s: A string
-     * @return: A list of lists of string
+     * @param matrix: a matrix of integers
+     * @param k: an integer
+     * @return: the kth smallest number in the matrix
      */
-    vector<vector<string>> partition(string s) {
-        // write your code here
-        vector<vector<string> > res;
-        vector<string> tmp;
-        
-        // use a dfs helper
-        dfs(res,tmp,s,0);
-        
-        return res;
-
+    //a node for matrix node
+    struct HeapNode{
+        int val;
+        int r;
+        int c;
+        HeapNode(int v, int row, int col):val(v),r(row),c(col){}
+        HeapNode():val(0),r(0),c(0){}
+    };
+    void swap(HeapNode &a, HeapNode &b){
+        HeapNode c=a;
+        a=b;
+        b=c;
     }
-    void dfs(vector<vector<string> > &res, vector<string> &tmp, string &s, int index){
-        if(index==s.size()){
-            res.push_back(tmp);
-            return;
+    // min heapify on a HeapNode array
+    void minheapify(HeapNode a[], int i, int heapsize){
+        int l=2*i+1;
+        int r=2*i+2;
+        
+        int smallest=i;
+        if(l<heapsize && a[l].val<a[smallest].val){
+            smallest=l;
         }
         
-        for(int len=1; index+len<=s.size(); len++){
-            string ss=s.substr(index,len);
-            if(isPalin(ss)){
-                tmp.push_back(ss);
-                dfs(res,tmp,s,index+len);
-                tmp.pop_back();
-            }
+        if(r<heapsize && a[r].val<a[smallest].val){
+            smallest=r;
+        }
+        
+        if(smallest!=i){
+            swap(a[i],a[smallest]);
+            minheapify(a,smallest,heapsize);
         }
     }
     
-    bool isPalin(string &s){
-        int l=0;
-        int r=s.size()-1;
-        
-        while(l<r){
-            if(s[l++]!=s[r--]){
-                return false;
-            }
+    void buildminheap(HeapNode a[],int heapsize){
+        for(int i=(heapsize-1)/2; i>=0; i--){
+            minheapify(a,i,heapsize);
         }
-        return true;
+    }
+    int kthSmallest(vector<vector<int> > &matrix, int k) {
+        // write your code here
+        int n=matrix[0].size();
+        int m=matrix.size();
+        
+        if(k<0 || k>m*n){
+            return INT_MAX;
+        }
+        
+        //initial the first row as HeapNode row
+        HeapNode a[n];
+        for(int j=0; j<n; j++){
+            a[j]=HeapNode(matrix[0][j],0,j);
+        }
+        
+        //buildminheap(a,n);
+        
+        HeapNode hr;
+        // iterate k times 
+        while(k--){
+            hr=a[0];
+            // push the same col next row val in the heap
+            int nextval=(hr.r+1<m)?matrix[hr.r+1][hr.c]:INT_MAX;
+            a[0]=HeapNode(nextval,hr.r+1,hr.c);
+            minheapify(a,0,n);
+        }
+        
+        return hr.val;
     }
 };
 
 
-
 int main(){
     Solution sln;
-    string s="bb";
+    vector<int> vec={1};
+    vector<int> avec={2};
+    vector<int> bvec={3};
+    vector<int> cvec={100};
+    vector<int> dvec={101};
+    vector<int> evec={1000};
+    vector<int> fvec={9999};
     
-    sln.partition(s);
+    
+    vector<vector<int> > v;
+    v.push_back(vec);
+    v.push_back(avec);
+    v.push_back(bvec);
+    v.push_back(cvec);
+    v.push_back(dvec);
+    v.push_back(evec);
+    v.push_back(fvec);
+   
+    
+    cout<<sln.kthSmallest(v,5);
 }
 
