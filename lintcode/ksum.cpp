6c92a0c6953ab2be45ff72bef8c398d7566377d1
Xingyu Chen <wizard_cxy@hotmail.com>
2016-02-22 09:44:55 +0800

    Update ksum.cpp

--- lintcode/ksum.cpp
+++ lintcode/ksum.cpp
@@ -11,20 +11,20 @@ public:
         int n=A.size();
         
         vector<vector<vector<int>>> d(k+1,vector<vector<int>>(n+1,vector<int>(target+1,0)));
-        // dp[i][j][v] means the ways of selecting i elements from first j element which
-        // sums to k
+        // d[i][j][v] means the ways of selecting i elements from first j elements which
+        // sums to v
         
         for(int i=1; i<=n; i++){
             if(A[i-1]<=target){
                 // not overflow
                 for(int j=i; j<=n; j++){
-                    // selecting 1 element from j(>=i elment ways is 1)
+                    // selecting 1 element from j(>=i element ways is 1)
                    d[1][j][A[i-1]]=1;
                 }
             }
         }
         //d[i][j][v]=d[i-1][j-1][v-A[j-1]+d[i][j-1][v]
-        // it means two ops first is select j-th element.second is not select it
+        // it means two ops, first is select j-th element.second is not select it
         for(int i=2; i<=k; i++){
             for(int j=i; j<=n; j++){
                 for(int v=1; v<=target; v++){
