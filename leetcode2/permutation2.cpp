7a8d59ef870201935420881a65e2d6ff78f7ed95
chenxingyu <wizard_cxy@hotmail.com>
2016-08-14 13:48:21 +0800

    update some comment

--- leetcode2/permutation2.cpp
+++ leetcode2/permutation2.cpp
@@ -21,6 +21,9 @@ public:
         for(int i=0; i<nums.size(); i++){
             if(used[i] || i>0 && nums[i]==nums[i-1] && used[i-1]){
                 // nums[i-1] is used and is the same with nums[i]
+                // tricky part is if input is 1 1 2
+                // we can use 1a 1b to distinguish the first two 1
+                // then the first result will be 1b 1a 2 using this algorithm
                 continue;
             }
             used[i]=1;
