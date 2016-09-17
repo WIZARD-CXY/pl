224d875ee73e9dd17a6ec77ce367d63eb24702b4
chenxingyu <wizard_cxy@hotmail.com>
2016-09-17 22:38:27 +0800

    update valid tree

--- lintcode/subtree.cpp
+++ lintcode/subtree.cpp
@@ -16,21 +16,6 @@ public:
      * @param T1, T2: The roots of binary tree.
      * @return: True if T2 is a subtree of T1, or false.
      */
-    bool helper(TreeNode *T1, TreeNode *T2){
-        if(T1==NULL && T2==NULL){
-            return true;
-        }
-        if(T1==NULL || T2==NULL){
-            return false;
-        }
-        
-        if (T1->val != T2->val) {
-            return false;
-        }  
-        return helper(T1->left,T2->left) && helper (T1->right,T2->right);
-         
-          
-    }
     bool isSubtree(TreeNode *T1, TreeNode *T2) {
         // write your code here
         if(T2==NULL){
@@ -42,15 +27,30 @@ public:
         }
         
         bool res=false;
-        if(T1->val==T2->val){
-            //check the sub tree if equal
-            res=helper(T1,T2);
+        if(T1->val == T2->val){
+            res=helper(T1, T2);
         }
         
+        // T1->val != T2->val check the T1's left sub-tree or right subtree if match
         if(!res){
-            res = isSubtree(T1->left,T2) || isSubtree(T1->right,T2);
+            res = isSubtree(T1->left, T2) || isSubtree(T1->right,T2);
+        }
+        return res;
+    }
+    
+    bool helper(TreeNode *T1, TreeNode *T2){
+        if(T1==NULL && T2==NULL){
+            return true;
+        }
+        //one is NULL and the other is not NULL definitely false
+        if(T1==NULL || T2==NULL){
+            return false;
         }
         
-        return res;
+        if(T1->val==T2->val){
+            return helper(T1->left,T2->left) && helper(T1->right,T2->right);
+        }
+        
+        return false;
     }
 };
