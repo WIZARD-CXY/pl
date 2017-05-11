c0f1b31e2859423df61d8dee6c642ce3cc9ebfe9
chenxingyu <wizard_cxy@hotmail.com>
2017-05-11 09:41:25 +0800

    update lis

--- lintcode/longestincreasingsequence.cpp
+++ lintcode/longestincreasingsequence.cpp
@@ -6,19 +6,20 @@ public:
      */
     int longestIncreasingSubsequence(vector<int> nums) {
         // write your code here
+        int n= nums.size();
         
-        int n=nums.size();
         if(n==0){
             return 0;
         }
-        // initial dp with 1 means at least length is 1
+        
+        // dp[i] denotes the lis size ending with nums[i]
+        // originallu every dp[i] is 1
         vector<int> dp(n,1);
         int res=1;
-        // dp[i] means the longest len of num[0,i] including end
         
         for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[j]<=nums[i] && 1+dp[j] > dp[i]){
+            for (int j=0;j<i; j++){
+                if (nums[j]<nums[i] && 1+dp[j]>dp[i]){
                     dp[i]=1+dp[j];
                 }
             }
@@ -27,6 +28,6 @@ public:
         }
         
         return res;
+        
     }
 };
-
